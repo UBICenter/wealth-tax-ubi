c99cf8903636e5ab68882189bd688bf81bfff7c8
Max Ghenis <mghenis@gmail.com>
2019-10-15 20:06:02 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='wealthtaxubi',
+      version='0.1',
+      description='Analysis and tool showing the effects on wealth inequality of a wealth-tax-funded UBI.',
+      url='http://github.com/UBICenter/wealth-tax-ubi',
+      author='Max Ghenis',
+      author_email='max@ubicenter.org',
+      license='MIT',
+      packages=['wealthtaxubi'],
+      install_requires=[
+          'microdf',
+          'numpy',
+          'pandas',
+      ],
+      zip_safe=False)
